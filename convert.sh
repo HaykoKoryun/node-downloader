4efa29cb487ae5b6c5ba04ba84d9ae6b83c779ad
HaykoKoryun <haykokoryun@gmail.com>
2017-04-06 19:12:56 +0400

    made test wait for file to be ready based on timestamp

--- convert.sh
+++ convert.sh
@@ -2,5 +2,4 @@
 
 # with a bash for loop
 printf "file '%s'\n" ./process/*.ts > convert.txt
-
-F:\_apps\ffmpeg\bin\ffmpeg -f concat -safe 0 -i convert.txt -c copy output.mp4
+F:\__apps\ffmpeg\bin\ffmpeg -f concat -safe 0 -i convert.txt -c copy output.mp4
--- test.js
+++ test.js
@@ -4,54 +4,72 @@ var fs = require("fs");
 
 require('draftlog').into(console)
 
-var q = async.queue(function(part, cb)
-{
-  var update = console.draft(part.file + ": starting");
-  var finished = true;
-  
-  function downloadPart()
-  {		  
-	request
-	  .get(part.url)
-	  .on('response', function(response)
+var q = async.queue
+(
+	function(part, cb)
+	{
+	  var update = console.draft(part.file + ": starting");
+	  var finished = true;
+	  
+	  function check()
 	  {
-		if(response.statusCode != 200)
+		var now = Date.now();
+		if(now/10000 > part.time)
 		{
-			finished = false;
+			setTimeout(downloadPart, 500);
 		}
 		else
 		{
-			finished = true;
-			var len = parseInt(response.headers['content-length'], 10);
-		var cur = 0;
-
-		response.on("data", function(chunk)
-		{
-		  cur += chunk.length;
-		  update(part.file + ": downloading " + (100.0 * cur / len).toFixed(2) + "%");
-		});
+			update(part.file + ": too soon, retrying in 20 seconds ");
+			setTimeout(check, 20000);
 		}
-	  })
-	  .on('error', function(err)
-	  {
-	  })
-	  .on('end', function()
-	  {
-		  if(!finished)
+	  }
+	  
+	  function downloadPart()
+	  {		  
+		request
+		  .get(part.url)
+		  .on('response', function(response)
 		  {
-			  update(part.file + ": error, retrying in 18 seconds ");
-			setTimeout(downloadPart, 18000);
-		  }
-		  else
+			if(response.statusCode != 200)
+			{
+				finished = false;
+			}
+			else
+			{
+				finished = true;
+				var len = parseInt(response.headers['content-length'], 10);
+				var cur = 0;
+
+				response.on("data", function(chunk)
+				{
+				  cur += chunk.length;
+				  update(part.file + ": downloading " + (100.0 * cur / len).toFixed(2) + "%");
+				});
+			}
+		  })
+		  .on('error', function(err)
 		  {
-			  setTimeout(cb, 9000);
-		  } 
-	  })
-	  .pipe(fs.createWriteStream("parts/" + part.file));
-}
-  
-  downloadPart();
-}, 10);
+		  })
+		  .on('end', function()
+		  {
+			  if(!finished)
+			  {
+				  update(part.file + ": error, retrying in 20 seconds ");
+				setTimeout(downloadPart, 20000);
+			  }
+			  else
+			  {
+				  setTimeout(cb, 9000);
+			  } 
+		  })
+		  .pipe(fs.createWriteStream("parts/" + part.file));
+		}
+		
+		check();
+	},
+	2
+);
 
 q.drain = function()
 {
@@ -60,12 +78,12 @@ q.drain = function()
 
 (function()
 {
-	var startindex = 149079665;
+	var startindex = 149148700;
 	var limit = 600;
 	
 	for(var i = 0; i < limit; ++i)
 	{
-		var url = "https://livestream-f.akamaihd.net/i/14968495_4949630_lsicm7i5lw1qes77kdg_1@414355/segment" + (startindex + i) + "_2128_av-p.ts?sd=10&dw=14400&rebase=on&hdntl=exp=1490885391~acl=/i/14968495_4949630_lsicm7i5lw1qes77kdg_1@414355/*~data=hdntl~hmac=3e0181af5d31acae04fb252983f395d31679f1502d5556805be47a8692ee73a6";
+		var url = "https://livestream-f.akamaihd.net/i/14968495_4949630_lsi0nz1zo1nl87trbj4_1@305406/segment" + (startindex + i) + "_2128_av-p.ts?sd=10&dw=14400&rebase=on&hdntl=exp=1491573920~acl=/i/14968495_4949630_lsi0nz1zo1nl87trbj4_1@305406/*~data=hdntl~hmac=059c4a67db5b3a4ba1d0bc5d59dcc555dd7e58f0373db6cd0a6c6e1462ca72ef";
 		
 		var file = url.match(/(segment.+?\.ts)/)[0];
 		
@@ -73,7 +91,8 @@ q.drain = function()
 		  (
 			{
 			  url: url,
-			  file: file
+			  file: file,
+			  time: (startindex + i)
 			},
 			function(err)
 			{
